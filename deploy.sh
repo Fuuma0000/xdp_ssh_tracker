975083ba6e294886257459ffcb56f1014ac2ade8
Fuuma <masa81hiro69@gmail.com>
2025-03-22 18:42:18 +0900

    fix/add-BTF-opttion

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-clang -O2 -target bpf -c xdp_ssh_tracker.c -o xdp_ssh_tracker.o
+clang -g -O2 -target bpf -c xdp_ssh_tracker.c -o xdp_ssh_tracker.o
 sudo ip link set dev enp2s0 xdp off
 sudo ip link set dev enp2s0 xdp obj xdp_ssh_tracker.o sec xdp
